0349ed006ada2b9d654e0cf6e27a3388e21a8dc4
MWQJ0 <90338687+MWQJ0@users.noreply.github.com>
2021-09-09 18:30:10 +0300

    Add files via upload

---
+++ train_fcn_cluster.sh
@@ -0,0 +1,51 @@
+#@IgnoreInspection BashAddShebang
+#   This is the most basic QSUB file needed for this cluster.
+#   Further examples can be found under /share/apps/examples
+#   Most software is NOT in your PATH but under /share/apps
+#
+#   For further info please read http://hpc.cs.ucl.ac.uk
+#   For cluster help email cluster-support@cs.ucl.ac.uk
+#
+#   NOTE hash dollar is a scheduler diredctive not a comment.
+
+
+# These are flags you must include - Two memory and one runtime.
+# Runtime is either seconds or hours:min:sec
+
+#$ -S /bin/bash
+#$ -l tmem=8G
+# -l h_vmem=2G -- with pytorch, omit this flag
+#$ -l h_rt=27:00:00
+#$ -l gpu=true
+# -pe gpu 2
+# -R y
+# -l hostname=gonzo*
+#These are optional flags but you problably want them in all jobs
+
+#$ -j y
+#$ -N train_fcn8s_0.8_19aug_coeff10
+#$ -o /cluster/project9/MUNIT/segmentation_kyveli/segmentation
+
+
+hostname
+date
+
+export PATH=/home/ktsioli/miniconda3/bin:$PATH
+
+source activate python3.6_pytorch0.4.1
+
+
+python /cluster/project9/MUNIT/segmentation_kyveli/segmentation/train_fcn.py \
+    --outdir /cluster/project9/MUNIT/segmentation_kyveli/segmentation/results/train_fcn8s_0.8_19aug_coeff10/cityscapes_fcn8s \
+    --logdir /cluster/project9/MUNIT/segmentation_kyveli/segmentation/runs/train_fcn8s_0.8_19aug_coeff10\
+    --model fcn8s \
+    --num_cls 19 \
+    --lr 1e-3 \
+    --batch_size 1 \
+    --p_synthetic 0.8 \
+    --momentum 0.9 \
+    --crop_size 512 \
+    --iterations 100000 \
+    --datadir '/cluster/project9/MUNIT/segmentation_kyveli' \
+    --dataset cityscapes \
+--config_path /cluster/project9/MUNIT/segmentation_kyveli/segmentation/gta2cityscapes_folder_MUNIT.yaml \
